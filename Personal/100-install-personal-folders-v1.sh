9d595235811bc8e856e7daaa7b0a3cfc1cba8028
Erik Dubois <erik.dubois@gmail.com>
2018-03-13 07:12:56 +0100

    personal folders cleanup

--- Personal/100-install-personal-folders-v1.sh
+++ Personal/100-install-personal-folders-v1.sh
@@ -13,31 +13,15 @@ set -e
 #
 ##################################################################################################################
 
-echo "Creating all folders"
-echo "Creates only folders if they do not exist"
+echo "Creating common folders in correct language"
+
+xdg-user-dirs-update --force
+
+echo "Creating private folders we use later"
 
 [ -d $HOME"/.icons" ] || mkdir -p $HOME"/.icons"
-[ -d $HOME"/.config/gtk-3.0" ] || mkdir -p $HOME"/.config/gtk-3.0"
-[ -d $HOME"/.config/conky" ] || mkdir -p $HOME"/.config/conky"
-[ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
-[ -d $HOME"/.config/Thunar" ] || mkdir -p $HOME"/.config/Thunar"
-[ -d $HOME"/.config/xfce4" ] || mkdir -p $HOME"/.config/xfce4"
 [ -d $HOME"/.themes" ] || mkdir -p $HOME"/.themes"
-[ -d $HOME"/Desktop" ] || mkdir -p $HOME"/Desktop"
-[ -d $HOME"/Documents" ] || mkdir -p $HOME"/Documents"
-[ -d $HOME"/Downloads" ] || mkdir -p $HOME"/Downloads"
-[ -d $HOME"/DATA" ] || mkdir -p $HOME"/DATA"
-#[ -d $HOME"/Dropbox" ] || mkdir -p $HOME"/Dropbox" will be created by dropbox
-[ -d $HOME"/Insync" ] || mkdir -p $HOME"/Insync"
-[ -d $HOME"/Music" ] || mkdir -p $HOME"/Music"
-[ -d $HOME"/Pictures" ] || mkdir -p $HOME"/Pictures"
-[ -d $HOME"/Videos" ] || mkdir -p $HOME"/Videos"
 [ -d $HOME"/.fonts" ] || mkdir -p $HOME"/.fonts"
-[ -d $HOME"/.gimp-2.8" ] || mkdir -p $HOME"/.gimp-2.8"
-[ -d $HOME"/.gimp-2.8/scripts" ] || mkdir -p $HOME"/.gimp-2.8/scripts"
-[ -d $HOME"/.gimp-2.8/themes" ] || mkdir -p $HOME"/.gimp-2.8/themes"
-
-
 
 echo "################################################################"
 echo "###       personal folders created or existed already       ####"
