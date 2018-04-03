6376c7869b49bc97c0073fe2398af7a97869aa5f
Erik Dubois <erik.dubois@gmail.com>
2018-04-03 19:33:48 +0200

    imagemagick and w3m

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -41,6 +41,8 @@ echo "Installing category Other"
 echo "Installing category System"
 
 sudo pacman -S --noconfirm --needed dmenu
+sudo pacman -S --noconfirm --needed imagemagick
+sudo pacman -S --noconfirm --needed w3m 
 sudo pacman -S --noconfirm --needed slim
 sudo pacman -S --noconfirm --needed xfce4-notifyd
 
