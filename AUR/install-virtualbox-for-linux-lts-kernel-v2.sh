987e91d3669dd068449416517dcb98054d06ab32
Erik Dubois <erik.dubois@gmail.com>
2018-04-28 12:21:33 +0200

    new number for virtualbox

---
+++ AUR/install-virtualbox-for-linux-lts-kernel-v2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S --noconfirm --needed virtualbox
+sudo pacman -S --noconfirm --needed virtualbox-host-dkms
+sudo pacman -S --noconfirm --needed linux-lts-headers
+
+echo "################################################################"
+echo "#########           You got to reboot.                 #########"
+echo "################################################################"
