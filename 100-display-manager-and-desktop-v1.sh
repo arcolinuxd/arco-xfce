b3f15f526d18857b87f6ff23b92aa2b2a37ec381
Erik Dubois <erik.dubois@gmail.com>
2018-04-29 13:47:24 +0200

    better alignment

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -25,7 +25,7 @@ sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
 
 
-echo "Remove anything you do not like from the installed applications"
+#Remove anything you do not like from the installed applications
 
 #sudo pacman -R ...
 sudo pacman -R xfce4-artwork --noconfirm
