c03b39c89a593bca5cc936061efa6b71d62bc8d0
Erik Dubois <erik.dubois@gmail.com>
2018-09-04 09:58:57 +0200

    arc-gtk-theme moved

--- 300-software-AUR-repo-v1.sh
+++ 300-software-AUR-repo-v1.sh
@@ -65,7 +65,6 @@ echo "Installing category Other"
 
 echo "Installing category System"
 
-sh AUR/install-arc-gtk-theme-git-v*.sh
 sh AUR/install-downgrade-v*.sh
 sh AUR/install-inxi-v*.sh
 sh AUR/install-neofetch-v*.sh
