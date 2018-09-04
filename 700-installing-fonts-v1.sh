3b81f987dd5145d6f12af95bd8307fc6eeca6de3
Erik Dubois <erik.dubois@gmail.com>
2018-09-04 10:08:26 +0200

    update

--- 700-installing-fonts-v1.sh
+++ 700-installing-fonts-v1.sh
@@ -13,7 +13,7 @@ set -e
 #
 ##################################################################################################################
 
-echo "Installing fonts"
+echo "Installing fonts from Arch Linux repo"
 
 sudo pacman -S adobe-source-sans-pro-fonts --noconfirm --needed
 sudo pacman -S cantarell-fonts --noconfirm --needed
@@ -29,5 +29,27 @@ sudo pacman -S ttf-ubuntu-font-family --noconfirm --needed
 sudo pacman -S tamsyn-font --noconfirm --needed
 
 echo "################################################################"
-echo "####             Fonts have been installed                  ####"
+echo "####        Fonts from Arch Linux repo have been installed        ####"
+echo "################################################################"
+
+
+echo "################################################################"
+echo "####        Installing fonts for conkies                    ####"
+echo "################################################################"
+
+[ -d $HOME"/.fonts" ] || mkdir -p $HOME"/.fonts"
+
+
+echo "Copy fonts to .fonts"
+
+cp Personal/settings/fonts/* ~/.fonts/
+
+echo "Building new fonts into the cache files";
+echo "Depending on the number of fonts, this may take a while..."
+fc-cache -fv ~/.fonts
+
+
+
+echo "################################################################"
+echo "#########   Fonts have been copied and loaded   ################"
 echo "################################################################"
