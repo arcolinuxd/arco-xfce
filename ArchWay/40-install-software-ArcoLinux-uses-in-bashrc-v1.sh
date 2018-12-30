9888545d32435bb11b7a9fd89c266d6b9c8f43f7
Erik Dubois <erik.dubois@gmail.com>
2018-12-30 18:46:13 +0100

    more website info

--- ArchWay/40-install-software-ArcoLinux-uses-in-bashrc-v1.sh
+++ ArchWay/40-install-software-ArcoLinux-uses-in-bashrc-v1.sh
@@ -14,17 +14,15 @@ set -e
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-# change into your name and email.
 
-git init
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
-sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=25000'
-git config --global push.default simple
+echo "Installing the software ArcoLinux uses in .bashrc"
+
+sudo pacman -S --noconfirm --needed expac
+sudo pacman -S --noconfirm --needed hwinfo
+sudo pacman -S --noconfirm --needed reflector
+sudo pacman -S --noconfirm --needed youtube-dl
 
 
 echo "################################################################"
-echo "###################    T H E   E N D      ######################"
+echo "###                  software installed                     ####"
 echo "################################################################"
