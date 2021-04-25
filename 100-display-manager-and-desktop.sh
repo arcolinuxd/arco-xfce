7b8623feb3a672ddfc62df073a02a8c97cb0736f
Erik Dubois <erik.dubois@gmail.com>
2021-04-25 19:34:04 +0200

    lightdm replaces with sddm

--- 100-display-manager-and-desktop.sh
+++ 100-display-manager-and-desktop.sh
@@ -47,9 +47,7 @@ echo "Installation of the core software"
 ###############################################################################
 
 list=(
-lightdm
-arcolinux-lightdm-gtk-greeter
-arcolinux-lightdm-gtk-greeter-settings
+sddm
 arcolinux-wallpapers-git
 xfce4
 xfce4-goodies
@@ -74,10 +72,10 @@ cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
 cp -arf /etc/skel/. ~
 
 tput setaf 5;echo "################################################################"
-echo "Enabling lightdm as display manager"
+echo "Enabling sddm as display manager"
 echo "################################################################"
 echo;tput sgr0
-sudo systemctl enable lightdm.service -f
+sudo systemctl enable sddm.service -f
 
 tput setaf 2;echo "################################################################"
 echo "Removing packages we do not want"
